d825d20c37099c303bc2f5d183fceb68f26c80bb
JaHe03 <jarynh14@gmail.com>
2025-08-02 18:42:50 -0400

    restart

--- frontend/my-expo-app/App.tsx
+++
@@ -1,8 +0,0 @@
-import { ExpoRoot } from "expo-router";
-import { registerRootComponent } from "expo";
-import "nativewind";
-
-// @ts-expect-error
-const App = () => <ExpoRoot context={require.context("./app")} />;
-
-export default registerRootComponent(App);
